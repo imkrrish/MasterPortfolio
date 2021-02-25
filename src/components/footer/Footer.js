8a45b0577b00202664c9286f95e8b653a474eff1
imkrrish <imkrrish227@gmail.com>
2021-02-25 21:01:33 +0530

    intial commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+import { Fade } from "react-reveal";
+import { greeting } from "../../portfolio.js";
+/* eslint-disable jsx-a11y/accessible-emoji */
+
+export default function Footer(props) {
+  return (
+    <div className="footer-div">
+      <Fade>
+        <p className="footer-text" style={{ color: props.theme.secondaryText }}>
+          {/* Made with <span role="img">❤️</span> by {greeting.title} */}
+          Special thanks to <a target=" " href="https://www.linkedin.com/in/imkrrish/">Krishan Kumar</a>
+        </p>
+        {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
+      </Fade>
+    </div>
+  );
+}
